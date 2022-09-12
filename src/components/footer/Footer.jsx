8b330219c13ea032f5e0a11a67a9a14eb74e01c4
Muhammad Umer <mumer119131@gmail.com>
2022-09-12 16:47:39 +0500

    solved some responsiveness issues

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,12 +9,12 @@ const Footer = () => {
   return (
     <footer>
       <h2>| Socials |</h2>
-      <div className="social__container">
-        <FaGithub />
-        <BsFacebook />
-        <AiFillInstagram />
-        <AiFillLinkedin />
-        <AiFillBehanceCircle />
+      <div className="social__container__footer" data-aos='slide-up'>
+        <a href="https://github.com/mumer119131" target="_blank"><FaGithub /></a>
+        <a href="https://www.facebook.com/profile.php?id=100083694507517" target="_blank"><BsFacebook /></a>
+        <a href="https://www.instagram.com/umer_sma119131/" target="_blank"><AiFillInstagram /></a>
+        <a href="https://www.linkedin.com/in/m-umer-06602821b/" target="_blank"><AiFillLinkedin /></a>
+        <a href="https://www.behance.net/mumer4" target="_blank"><AiFillBehanceCircle /></a>
       </div> 
     </footer>
   )
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './home.css'
 import MY_IMAGE from '../../assets/my.png'
 import CROWN_IMG from '../../assets/doodles/crown.png'
 import CV from '../../assets/cv.pdf'
 
-const CROWN_REF = React.createRef()
 
 const Home = () => {
-  
-  function rotateCrown(){
-    console.log(CROWN_REF)
+  const CROWN_REF = useRef()
+
+  function scrollDown(){
+    window.scrollTo(0,document.body.scrollHeight)
   }
   return (
-    <div className="container home__container" onScroll={rotateCrown()}>
+    <div className="container home__container">
       <small>Hello I'm</small>
       <h2><img src={CROWN_IMG} alt="" ref={CROWN_REF}/> Muhammad Umer</h2>
       <p>- FullStack Developer -</p>
@@ -30,7 +30,7 @@ const Home = () => {
         <a href="https://www.facebook.com/profile.php?id=100083694507517" target="_blank"><i className="fa-brands fa-facebook"></i></a>
       </div>
       <div className="side__bar scroll__container">
-        <p>Scroll Down</p>
+        <p onClick={scrollDown}>Scroll Down</p>
       </div>
     </div>
   )
